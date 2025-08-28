1e907b147b5ff30e14b02b11d63d830e864768ae
NeoRise456 <defneorise@gmail.com>
2025-08-28 02:32:54 -0500

    feat: add custom footer component

---
+++ components/CustoFooter.tsx
@@ -0,0 +1,12 @@
+export default function CustoFooter(){
+    return (
+        <footer className="rounded-2xl h-auto min-h-[5.5rem] px-16  text-white font-medium bg-gradient-to-b from-[#0e121b] from-[38.93%] to-[#2b303b] to-[80.86%] overflow-hidden relative">
+
+            <div className={`w-full max-w-[67rem] h-full mx-auto flex justify-center md:justify-end items-center`}>
+                <span>
+                    2025 © Juan Astonitas
+                </span>
+            </div>
+        </footer>
+    );
+}
